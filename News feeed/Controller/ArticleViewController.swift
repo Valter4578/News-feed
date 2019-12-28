0d70000e69fac46ea5dc01fefdf180d4258846b3
Максим Алексеев <mrvalter566@gmail.com>
2019-12-28 12:02:00 +0700

    Create groups and create files for 2 view controllers

---
+++ News feeed/Controller/ArticleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ArticleViewController.swift
+//  News feeed
+//
+//  Created by Максим Алексеев on 28.12.2019.
+//  Copyright © 2019 Максим Алексеев. All rights reserved.
+//
+
+import UIKit
+
+class ArticleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ News feeed/Controller/MainTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainTableViewController.swift
+//  News feeed
+//
+//  Created by Максим Алексеев on 28.12.2019.
+//  Copyright © 2019 Максим Алексеев. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+
+}
