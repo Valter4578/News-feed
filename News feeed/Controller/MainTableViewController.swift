bf5b9a76a2cfa19876d91f1da083caa0db616ad5
Максим Алексеев <mrvalter566@gmail.com>
2020-01-02 12:07:52 +0700

    Change apiURL

--- News feeed/Controller/MainTableViewController.swift
+++ News feeed/Controller/MainTableViewController.swift
@@ -9,15 +9,24 @@
 import UIKit
 
 class MainTableViewController: UITableViewController {
-
-    var apiUrl: URL?
+    // MARK: - Dependencies
+    var networkService: NetworkService?
+    // MARK: - Public properties
+    var articles: Articles?
+    // MARK: - Constants
     
+    // MARK: - Private properties
+    private var apiUrl: URL?
+    // MARK: - Lyfecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let networkService = NetworkService()
-        apiUrl = URL(string: "https://newsapi.org/v2/everything?q=bitcoin&from=2019-11-30&sortBy=publishedAt&apiKey=c15b2503eabf49ffb370b739e4c995f2")
-        networkService.parseJSON(from: apiUrl!)
+        
+        networkService = NetworkService()
+        apiUrl = URL(string: "https://newsapi.org/v2/everything?q=bitcoin&from=2019-12-02&sortBy=publishedAt&apiKey=c15b2503eabf49ffb370b739e4c995f2")
+        networkService?.parseJSON(from: apiUrl!)
+        
+        networkService?.delegate = self
     }
 
     // MARK: - Table view data source
@@ -31,12 +40,20 @@ class MainTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "main cell", for: indexPath) as? MainTableViewCell else { return UITableViewCell() }
+        
+        cell.articleDescription.text = articles?.articles?[indexPath.row].description
         return cell
     }
 
 
+}
+
+extension MainTableViewController: NetworkServiceDelegate {
+
+    func transferArticleData(data: Articles) {
+        articles = data
+    }
+    
+    
 }
